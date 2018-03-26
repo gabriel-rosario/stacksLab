b170beb86dbb601d481dcd291a2cc6d135930bc7
ICOM STUDENTS <4035b13@icaco.ece.uprm.edu>
2018-03-26 12:47:30 -0400

    exercise 1 done

--- src/classes/SymmetricStringAnalyzer.java
+++ src/classes/SymmetricStringAnalyzer.java
@@ -12,20 +12,56 @@ public class SymmetricStringAnalyzer {
 	 */
 	public boolean isValidContent() { 
 		// ADD MISSING CODE
-		
-		return true;  // need to change if necessary....
+		SLLStack<Character> stack = new SLLStack<Character>(); 
+	    for (int i=0; i < s.length(); i++) { 
+	        char c = s.charAt(i); 
+	        
+	        if(Character.isLetter(c)){
+	        	if(Character.isUpperCase(c)){
+	        		stack.push(c);
+	        	}else if(stack.isEmpty()){
+	        		return false;
+	        	}else{
+	        		char t = stack.top();
+	        		if(t == Character.toUpperCase(c)){
+	        			stack.pop();
+	        		}else{
+	        			return false;
+	        		}
+	        	}
+	        }else{
+	        	return false;
+	        }
+	    }
+	    
+	    if(stack.isEmpty()){
+	    	return true;
+	    }
+	    return false;
 	}
+
 	
 	public String toString() { 
 		return s; 
 	}
 
-	public String parenthesizedExpression() 
-	throws StringIsNotSymmetricException 
+	public String parenthesizedExpression() throws StringIsNotSymmetricException 
 	{
 		// ADD MISSING CODE
+		if(!isValidContent()){
+			throw new StringIsNotSymmetricException("String is not symmetric");
+		}
+		String str = "";
+		for(int i = 0; i<s.length();i++){
+			char c = s.charAt(i);
+			if(Character.isUpperCase(c)){
+				str += "<" + c+" ";
+			}else{
+				str += c + "> ";
+			}
+		}
+	    return str;	
 		
-		return null;  // need to change if necessary....
 	}
 
 }
